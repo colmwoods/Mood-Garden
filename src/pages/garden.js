fccf130a1077055cbf3d25f04a151dc23f0f6d1c
11florin <albuflorin929@yahoo.co.uk>
2026-02-28 14:15:38 +0000

    Add function initGardenLogic()

--- src/pages/garden.js
+++ src/pages/garden.js
@@ -1,5 +1,26 @@
 export function renderGarden() {
-    document.getElementById('app').innerHTML = `
+  document.getElementById("app").innerHTML = `
     <h1>🌿 Garden</h1>
-  `
+    <div id="garden" class="garden"></div>
+    <div id="info" class="info-box"></div>
+    <button id="backBtn">Back to Home</button>
+  `;
+  initGardenLogic();
+}
+
+// ---------Page Logic -----------
+
+function initGardenLogic() {
+  // Retrive all saved mood entries from storage
+  const moods = getMoods();
+  const garden = document.getElementById('garden');
+
+  // Render a plant for each logged mood
+  renderAllPlants(moods, garden);
+
+  // Enable hoover behaviour for plant information
+  setUpHoover(garden);
+
+  // Enable navigation back to the home page
+  setupBackButton();
 }
